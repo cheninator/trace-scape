a4fbcbfd16573224fc0ddf655fa715a8ea063547
Yonni Chen <yonni.scholars@gmail.com>
2017-10-30 16:01:51 -0400

    Set up concurrently

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,15 +1,15 @@
-const electron = require('electron')
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path')
-const url = require('url')
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow: any;
 
 function createWindow () {
   // Create the browser window.
