fe284fb5314901e600596d1fee07250254e271f3
Yonni Chen <yonni.scholars@gmail.com>
2017-10-31 13:45:02 -0400

    Added a gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var flatten = require('gulp-flatten');
+var uglify = require('gulp-uglify');
+var buildPath = "./bin";
+
+var modules = [
+    'jquery/**/jquery.min.js',
+    'pixi.js/**/pixi.min.js'
+];
+
+var libs = [
+    'protobuf-3.4.0/google-protobuf.js',
+];
+
+/* External libraries */
+gulp.task("libs", () => {
+    return gulp.src(libs, {cwd: "libs/**"})
+        .pipe(flatten())
+		.pipe(gulp.dest(`${buildPath}/libs`));
+});
+
+/* Node modules */
+gulp.task("modules", () => {
+    return gulp.src(modules, {cwd: "node_modules/**"})
+        .pipe(flatten())
+		.pipe(gulp.dest(`${buildPath}/libs`));
+});
+
+gulp.task('templates', () => {
+	return gulp.src(['./src/**/*.html'])
+        .pipe(gulp.dest(buildPath));
+});
+
+gulp.task('build', [ 'templates', 'modules', 'libs' ]);
---
+++ src/common/protocol/trace-server-protocol.ts
@@ -0,0 +1,73 @@
+import * as $ from 'jquery';
+
+export interface ITraceServerProtocol {
+    fetchTree(): Promise<any>;
+    fetchEvents(filter: any): Promise<any>;
+    fetchArrows(): Promise<any>;
+}
+
+export class TraceServerProtocol implements ITraceServerProtocol {
+
+    private serverUrl_: string;
+    private traceId_: string;
+
+    constructor(serverUrl: string, traceId: string) {
+        this.serverUrl_ = serverUrl;
+        this.traceId_ = traceId;
+    }
+
+    set serverUrl(serverUrl: string) {
+        this.serverUrl_ = serverUrl;
+    }
+
+    set traceId(serverUrl: string) {
+        this.serverUrl_ = serverUrl;
+    }
+
+    fetchTree() : Promise<any> {
+        return new Promise((resolve, reject) => {
+            $.ajax(
+                {
+                    type: 'GET',
+                    url: `${this.serverUrl_}/tracecompass/traces/${this.traceId_}/ControlFlowView`,
+                    contentType: 'application/x-www-form-urlencoded',
+                    success: (response) => {
+                        let obj = JSON.parse(response);
+                        resolve(obj);
+                    },
+                    error: (xhr, status, error) => {
+                        reject(error);
+                    }
+                }
+            );
+        });
+    }
+
+    fetchEvents(filter: any) : Promise<any> {
+        return new Promise((resolve, reject) => {
+            let req = new XMLHttpRequest();
+            req.open('POST', `${this.serverUrl_}/tracecompass/traces/${this.traceId_}/ControlFlowView/events`, true);
+            req.setRequestHeader('Content-type', 'application/json');
+            req.responseType = 'arraybuffer';
+
+            req.onload = (oEvent) => {
+                // Not oReq.responseText
+                let arrayBuffer = req.response;
+                if (arrayBuffer) {
+                    let byteArray = new Uint8Array(arrayBuffer);
+                    resolve(byteArray);
+                }
+            };
+
+            req.onerror = (error) => {
+                reject(error);
+            };
+
+            req.send(JSON.stringify(filter));
+        });
+    }
+
+    fetchArrows() : Promise<any> {
+        return null;
+    }
+}
