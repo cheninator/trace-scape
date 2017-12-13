174fbe5abd4fcb1b99cdaa6f60f09c93ee3a3bc2
Yonni Chen <yonni.scholars@gmail.com>
2017-12-12 22:47:15 -0500

    Improve tests

--- test/test-cpu-usage.spec.ts
+++ test/test-cpu-usage.spec.ts
@@ -13,50 +13,126 @@ import { XYRequestFilter } from './../src/common/filter/xy-request-filter';
 import { BaseRequestFilter } from '../src/common/filter/base-request-filter';
 import { Utils } from '../src/common/utils';
 
-describe('CPU Usage model provider', () => {
+const DEFAULT_REPETITION = 10;
+
+describe('CPU usage model provider', () => {
 
     let serverUrl = 'http://localhost:8080/tracecompass';
     let trace: Trace;
 
     before(async () => {
         let traceModelProvider = new TraceModelProvider(serverUrl);
-        let name = 'many-threads';
+        let name = 'kernel_vm';
         let path = `/home/yonni/Documents/traces/ctf/src/main/resources/${name}`;
         trace = await traceModelProvider.putTrace(name, path);
     });
 
-    it('queries 1000 times the disk io activity endpoint', async () => {
-        let times = new Array();
-        let modelProvider = new CpuUsageModelProvider(serverUrl, trace);
+    /*
+     * Queries only total series for the CPU usage endpoint
+     */
+    it('queries 10 xy points to the CPU usage endpoint from trace start to trace end', async (done) => {
+        testXYPoints(10, 20);
+        done();
+    });
 
-        let baseFilter: BaseRequestFilter = {
-            start: trace.start,
-            end: trace.end,
-            count: 100
-        };
+    /*
+     * Queries only total series for the CPU usage endpoint
+     */
+    it('queries 100 xy points to the CPU usage endpoint from trace start to trace end', async (done) => {
+        testXYPoints(100, 20);
+        done();
+    });
 
+    /*
+     * Queries only total series for the CPU usage endpoint
+     */
+    it('queries 1000 xy points to the CPU usage endpoint from trace start to trace end', async (done) => {
+        testXYPoints(1000, 20);
+        done();
+    });
+
+    it('queries the xy entries to the CPU usage endpoint from trace start to trace end', async (done) => {
+        testXYEntries(100, 20);
+        done();
+    });
+
+    after(async () => {
+        // TODO : Clean ressources
+        await Utils.wait(100);
+    });
+
+    /*
+     * @param count: The number of XY points to fetch
+     * @param repeat: The number of time to query the endpoint. If not specified, 10 is default
+     */
+    async function testXYPoints(count: number, repeat?: number) {
         let filter: XYRequestFilter = {
             start: trace.start,
             end: trace.end,
-            count: 100,
+            count: count,
             ids: new Array()
         };
 
-        for (let i = 0; i < 20; ++i) {
-            await modelProvider.fetchEntries(baseFilter);
+        let times = new Array();
+        let modelProvider = new CpuUsageModelProvider(serverUrl, trace);
+
+        let iteration = repeat === undefined ? DEFAULT_REPETITION : repeat;
+        for (let i = 0; i < iteration; ++i) {
             let start = performance.now();
             await modelProvider.fetchData(filter);
             let stop = performance.now();
             times.push(stop - start);
-            console.log(times[i]);
-            await Utils.wait(100);
         }
+        printStats(`testXYPoints: ${count} points`, times);
+    }
 
+    /*
+     * @param count: The number of XY points to fetch
+     * @param repeat: The number of time to query the endpoint. If not specified, 10 is default
+     */
+    async function testXYEntries(count: number, repeat?: number) {
+        let filter: BaseRequestFilter = {
+            start: trace.start,
+            end: trace.end,
+            count: count,
+        };
+
+        let times = new Array();
+        let modelProvider = new CpuUsageModelProvider(serverUrl, trace);
+
+        let iteration = repeat === undefined ? DEFAULT_REPETITION : repeat;
+        for (let i = 0; i < iteration; ++i) {
+            let start = performance.now();
+            await modelProvider.fetchEntries(filter);
+            let stop = performance.now();
+            times.push(stop - start);
+        }
+        printStats(`testXYEntries`, times);
+    }
+
+    function printStats(title: string, times: Array<number>) {
         let sum = times.reduce((previous, current) => current += previous);
         let avg = sum / times.length;
+        console.log("----- " + title + " -----");
         console.log("Average time : " + avg + " ms.");
+        console.log("Median time : " + median(times) + " ms.");
         console.log("Min : " + Math.min.apply(null, times) + " ms.");
         console.log("Max : " + Math.max.apply(null, times) + " ms.");
-    });
-});
+        console.log("-------------------------------------------");
+    }
 
+    function median(values: Array<number>) {
+        values.sort((a, b) => {
+            return a - b;
+        });
+
+        let half = Math.floor(values.length / 2);
+
+        if (values.length % 2) {
+            return values[half];
+        }
+        else {
+            return (values[half - 1] + values[half]) / 2.0;
+        }
+    }
+});
--- test/test-kernel-memory.spec.ts
+++ test/test-kernel-memory.spec.ts
@@ -13,6 +13,8 @@ import { XYRequestFilter } from './../src/common/filter/xy-request-filter';
 import { BaseRequestFilter } from '../src/common/filter/base-request-filter';
 import { Utils } from '../src/common/utils';
 
+const DEFAULT_REPETITION = 10;
+
 describe('Kernel memory usage model provider', () => {
 
     let serverUrl = 'http://localhost:8080/tracecompass';
@@ -25,37 +27,32 @@ describe('Kernel memory usage model provider', () => {
         trace = await traceModelProvider.putTrace(name, path);
     });
 
+    /*
+     * Queries only total series for the kernel memory usage endpoint
+     */
+    it('queries 10 xy points to the kernel memory usage endpoint from trace start to trace end', async (done) => {
+        testXYPoints(10, 20);
+        done();
+    });
+
+    /*
+     * Queries only total series for the kernel memory usage endpoint
+     */
+    it('queries 100 xy points to the kernel memory usage endpoint from trace start to trace end', async (done) => {
+        testXYPoints(100, 20);
+        done();
+    });
+
+    /*
+     * Queries only total series for the kernel memory usage endpoint
+     */
     it('queries 1000 xy points to the kernel memory usage endpoint from trace start to trace end', async (done) => {
+        testXYPoints(1000, 20);
+        done();
+    });
 
-        let baseFilter: BaseRequestFilter = {
-            start: trace.start,
-            end: trace.end,
-            count: 10000
-        };
-
-        let filter: XYRequestFilter = {
-            start: trace.start,
-            end: trace.end,
-            count: 10000,
-            ids: new Array()
-        };
-
-        let times = new Array();
-        let modelProvider = new KernelMemoryModelProvider(serverUrl, trace);
-        for (let i = 0; i < 10; ++i) {
-            await modelProvider.fetchEntries(baseFilter);
-            let start = performance.now();
-            await modelProvider.fetchData(filter);
-            let stop = performance.now();
-            times.push(stop - start);
-            console.log(times[i]);
-        }
-
-        let sum = times.reduce((previous, current) => current += previous);
-        let avg = sum / times.length;
-        console.log("Average time : " + avg + " ms.");
-        console.log("Min : " + Math.min.apply(null, times) + " ms.");
-        console.log("Max : " + Math.max.apply(null, times) + " ms.");
+    it('queries the xy entries to the kernel memory usage endpoint from trace start to trace end', async (done) => {
+        testXYEntries(100, 20);
         done();
     });
 
@@ -63,4 +60,79 @@ describe('Kernel memory usage model provider', () => {
         // TODO : Clean ressources
         await Utils.wait(100);
     });
+
+    /*
+     * @param count: The number of XY points to fetch
+     * @param repeat: The number of time to query the endpoint. If not specified, 10 is default
+     */
+    async function testXYPoints(count: number, repeat?: number) {
+        let filter: XYRequestFilter = {
+            start: trace.start,
+            end: trace.end,
+            count: count,
+            ids: new Array()
+        };
+
+        let times = new Array();
+        let modelProvider = new KernelMemoryModelProvider(serverUrl, trace);
+
+        let iteration = repeat === undefined ? DEFAULT_REPETITION : repeat;
+        for (let i = 0; i < iteration; ++i) {
+            let start = performance.now();
+            await modelProvider.fetchData(filter);
+            let stop = performance.now();
+            times.push(stop - start);
+        }
+        printStats(`testXYPoints: ${count} points`, times);
+    }
+
+    /*
+     * @param count: The number of XY points to fetch
+     * @param repeat: The number of time to query the endpoint. If not specified, 10 is default
+     */
+    async function testXYEntries(count: number, repeat?: number) {
+        let filter: BaseRequestFilter = {
+            start: trace.start,
+            end: trace.end,
+            count: count,
+        };
+
+        let times = new Array();
+        let modelProvider = new KernelMemoryModelProvider(serverUrl, trace);
+
+        let iteration = repeat === undefined ? DEFAULT_REPETITION : repeat;
+        for (let i = 0; i < iteration; ++i) {
+            let start = performance.now();
+            await modelProvider.fetchEntries(filter);
+            let stop = performance.now();
+            times.push(stop - start);
+        }
+        printStats(`testXYEntries`, times);
+    }
+
+    function printStats(title: string, times: Array<number>) {
+        let sum = times.reduce((previous, current) => current += previous);
+        let avg = sum / times.length;
+        console.log("----- " + title + " -----");
+        console.log("Average time : " + avg + " ms.");
+        console.log("Median time : " + median(times) + " ms.");
+        console.log("Min : " + Math.min.apply(null, times) + " ms.");
+        console.log("Max : " + Math.max.apply(null, times) + " ms.");
+        console.log("-------------------------------------------");
+    }
+
+    function median(values: Array<number>) {
+        values.sort((a, b) => {
+            return a - b;
+        });
+
+        let half = Math.floor(values.length / 2);
+
+        if (values.length % 2) {
+            return values[half];
+        }
+        else {
+            return (values[half - 1] + values[half]) / 2.0;
+        }
+    }
 });
